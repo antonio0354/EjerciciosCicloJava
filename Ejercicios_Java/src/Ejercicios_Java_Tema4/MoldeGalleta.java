a790df94066730293561511ffe402aacb35456bf
Antonio <Antonio@EquipodeAntonio>
2018-10-08 19:12:53 +0200

    MoldeGalleta

---
+++ Ejercicios_Java/src/Ejercicios_Java_Tema4/MoldeGalleta.java
@@ -0,0 +1,19 @@
+package Ejercicios_Java_Tema4;
+
+public class MoldeGalleta {
+
+	public int numero;
+	
+	
+	public MoldeGalleta(int valor) {
+		numero=valor;
+		
+	}
+	
+	public void miNumero() {
+		System.out.println("Mi numero es: " + numero);
+		
+	}
+	
+	
+}
---
+++ Ejercicios_Java/src/Ejercicios_Java_Tema4/principalGalleta.java
@@ -0,0 +1,19 @@
+package Ejercicios_Java_Tema4;
+
+public class principalGalleta {
+
+	public static void main(String[] args) {
+		MoldeGalleta galleta1= new MoldeGalleta(1);
+		galleta1.miNumero();
+		MoldeGalleta galleta2= new MoldeGalleta(2);
+		galleta2.miNumero();
+		MoldeGalleta galleta3= new MoldeGalleta(3);
+		galleta3.miNumero();
+		MoldeGalleta galleta4= new MoldeGalleta(4);
+		galleta4.miNumero();
+		MoldeGalleta galleta5= new MoldeGalleta(5);
+		galleta5.miNumero();
+		
+	}
+
+}
