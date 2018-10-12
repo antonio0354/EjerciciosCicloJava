d6e2f932feeb748ebfa465e22edd617723844a83
Antonio <Antonio@EquipodeAntonio>
2018-10-12 12:47:41 +0200

    JuegoCartaMasAltaterminado

--- Ejercicios_Java/src/Ejercicios_Java_Tema3/JuegoCartaMasAlta.java
+++ Ejercicios_Java/src/Ejercicios_Java_Tema3/JuegoCartaMasAlta.java
@@ -6,23 +6,105 @@ public class JuegoCartaMasAlta {
 
 	public static void main(String[] args) {
 		int cartaOrdenador=1,cartaJugador,paloOrdenador,paloJugador;
-		JOptionPane.showInputDialog(null, "Quieres Jugar?");
+		String numeromayor10jugador=" ";
+		String numeromayor10ordenador=" ";
+		String palonombrejugador=" ";
+		String palonombreordenador=" ";
+		
+		JOptionPane.showMessageDialog(null, "Quieres Jugar?");
 		
 		
-		cartaOrdenador=(int) (Math.round((Math.random()*11+2)));
+		cartaOrdenador=(int) (Math.round((Math.random()*12+2)));
 		
 		paloOrdenador=(int) Math.round(Math.random()*3+1);
 		
 		
-		cartaJugador=(int) (Math.round((Math.random()*11+2)));
+		cartaJugador=(int) (Math.round((Math.random()*12+2)));
 		
 		paloJugador=(int) Math.round(Math.random()*3+1);
 		
 		
+		if(paloJugador==4) {
+			palonombrejugador="Picas";
+		}else {
+			if(paloJugador==3) {
+				palonombrejugador="Diamantes";
+				
+			}else {
+				if(paloJugador==2) {
+					palonombrejugador="Treboles";
+				}else {
+					if(paloJugador==1) {
+						palonombrejugador="Corazones";
+					}
+				}
+			}
+		}
+		
+		if(paloOrdenador==4) {
+			palonombreordenador="Picas";
+		}else {
+			if(paloOrdenador==3) {
+				palonombreordenador="Diamantes";
+				
+			}else {
+				if(paloOrdenador==2) {
+					palonombreordenador="Treboles";
+				}else {
+					if(paloOrdenador==1) {
+						palonombreordenador="Corazones";
+					}
+				}
+			}
+		}
 		
 		
-		JOptionPane.showMessageDialog(null, "Tu carta es: "+cartaJugador);
-		JOptionPane.showMessageDialog(null, "La carta es: "+cartaOrdenador);
+		if(cartaJugador==11) {
+			numeromayor10jugador="J";
+			JOptionPane.showMessageDialog(null, "Tu carta es: "+numeromayor10jugador+"\nEl palo es: "+palonombrejugador);
+		}else {
+			if(cartaJugador==12) {
+				numeromayor10jugador="Q";
+				JOptionPane.showMessageDialog(null, "Tu carta es: "+numeromayor10jugador+"\nEl palo es: "+palonombrejugador);
+			}else {
+				if(cartaJugador==13) {
+					numeromayor10jugador="K";
+					JOptionPane.showMessageDialog(null, "Tu carta es: "+numeromayor10jugador+"\nEl palo es: "+palonombrejugador);
+				}else {
+					if(cartaJugador==14) {
+						numeromayor10jugador="As";
+						JOptionPane.showMessageDialog(null, "Tu carta es: "+numeromayor10jugador+"\nEl palo es: "+palonombrejugador);
+					}else {
+						JOptionPane.showMessageDialog(null, "Tu carta es: "+cartaJugador+"\nEl palo es: "+palonombrejugador);
+					}
+				}
+			}
+		}
+		
+		
+		if(cartaOrdenador==11) {
+			numeromayor10ordenador="J";
+			JOptionPane.showMessageDialog(null, "La carta del ordenador es: "+numeromayor10ordenador+"\nEl palo es: "+palonombreordenador);
+		}else {
+			if(cartaOrdenador==12) {
+				numeromayor10ordenador="Q";
+				JOptionPane.showMessageDialog(null, "La carta del ordenador es: "+numeromayor10ordenador+"\nEl palo es: "+palonombreordenador);
+			}else {
+				if(cartaOrdenador==13) {
+					numeromayor10ordenador="K";
+					JOptionPane.showMessageDialog(null, "La carta del ordenador es: "+numeromayor10ordenador+"\nEl palo es: "+palonombreordenador);
+				}else {
+					if(cartaOrdenador==14) {
+						numeromayor10ordenador="As";
+						JOptionPane.showMessageDialog(null, "La carta del ordenador es: "+numeromayor10ordenador+"\nEl palo es: "+palonombreordenador);
+					}else {
+						JOptionPane.showMessageDialog(null, "La carta del ordenador es: "+cartaOrdenador+"\nEl palo es: "+palonombreordenador);
+					}
+				}
+			}
+		}
+		
+		
 		
 		if(cartaJugador>cartaOrdenador) {
 			JOptionPane.showMessageDialog(null, "Has ganado");
